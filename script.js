13e73f481d7ce60805db85a3946366d31d6527c9
shecodesng <54619358+shecodesng@users.noreply.github.com>
2025-05-01 20:04:15 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("signupBtn").addEventListener("click", function () {
+  window.location.href = "signup.html"; 
+  });
+  
+  document.getElementById("loginBtn").addEventListener("click", function () {
+    window.location.href = "login.html"; 
+  });
